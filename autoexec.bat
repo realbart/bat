3195973c1af43d14f5c761bc123762251c55d1ad
bart <bart@kemps.eu>
2026-03-16 00:19:39 +0100

    simpler autoexec.bat

--- autoexec.bat
+++ autoexec.bat
@@ -1,20 +1,2 @@
 :; exec /home/bart/.local/bin/bat -k "$0" "$@"
-@echo off
-setlocal EnableExtensions
-
-:: Vraag of we D: willen koppelen aan ~
-choice /C JN /N /M "Wil je station D: koppelen aan %HOME%? (J/N): "
-if errorlevel 2 goto END
-
-echo Koppelen D: aan %HOME%...
-
-:: Koppel D: aan ~
-subst D: "%HOME%"
-if errorlevel 1 (
-    echo [FOUT] Mislukt om D: te koppelen aan "%HOME%".
-) else (
-    echo [OK] D: gekoppeld aan "%HOME%".
-)
-
-:END
-endlocal
+subst d c:\home\bart
